b7ddde5471769c786964a0ef6ec8cde2bd493075
Ram <ramn0711@gmail.com>
2017-03-30 01:01:37 -0400

    Made changes in favor of minimalism.

---
+++ personal-website/js/custom.js
@@ -0,0 +1,10 @@
+$("#link_mail").on("click",function(){
+    window.location.href = "mailto:ramn0711@gmail.com"
+})
+$("#link_github").on("click",function(){
+    window.location.href = "https://github.com/EliteHex","_blank"
+})
+$("#link_linkedin").on("click",function(){
+    window.location.href = "https://www.linkedin.com/in/ram-narayanan-90820936/",
+    "_blank"
+})
